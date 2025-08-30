d2091b8c44787cb1fcc0eec8a14bcfa2f70fbb63
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2025-08-30 19:04:51 +0200

    Add first wrapper

---
+++ Sources/RebrickableClient/SimpleWrapper/Brix.swift
@@ -0,0 +1,14 @@
+public class Brix {
+    // Class implementation goes here
+    public var ApiKey: String = ""
+
+private let config = OpenAPIClientAPIConfiguration.shared
+
+
+    public init(apiKey: String) {
+        self.ApiKey = apiKey
+        config.customHeaders["Authorization"] = "key \(apiKey)"
+    }
+
+public func 
+}
