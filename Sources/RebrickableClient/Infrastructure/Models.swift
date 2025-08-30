2e018bcf0163105cdffa0c530fc42f7376131f23
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2025-08-30 17:01:38 +0200

    move classes

--- Sources/RebrickableClient/Infrastructure/Models.swift
+++ Sources/RebrickableClient/Infrastructure/Models.swift
@@ -93,27 +93,3 @@ public struct Response<T> {
 }
 
 extension Response: Sendable where T: Sendable {}
-
-public final class RequestTask: @unchecked Sendable {
-    private let lock = NSRecursiveLock()
-    private var task: URLSessionDataTaskProtocol?
-
-    func set(task: URLSessionDataTaskProtocol) {
-        lock.withLock {
-            self.task = task
-        }
-    }
-
-    func get() -> URLSessionDataTaskProtocol? {
-        lock.withLock {
-            task
-        }
-    }
-
-    public func cancel() {
-        lock.withLock {
-            task?.cancel()
-            task = nil
-        }
-    }
-}
---
+++ Sources/RebrickableClient/Infrastructure/OpenAPIClientAPIConfiguration.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+#if canImport(FoundationNetworking)
+    import FoundationNetworking
+#endif
+
+open class OpenAPIClientAPIConfiguration: @unchecked Sendable {
+    public var basePath: String
+    public var customHeaders: [String: String]
+    public var credential: URLCredential?
+    public var requestBuilderFactory: RequestBuilderFactory
+    public var apiResponseQueue: DispatchQueue
+    public var codableHelper: CodableHelper
+
+    public var successfulStatusCodeRange: Range<Int>
+
+    public var interceptor: OpenAPIInterceptor
+
+    public init(
+        basePath: String = "https://rebrickable.com",
+        customHeaders: [String: String] = [:],
+        credential: URLCredential? = nil,
+        requestBuilderFactory: RequestBuilderFactory = URLSessionRequestBuilderFactory(),
+        apiResponseQueue: DispatchQueue = .main,
+        codableHelper: CodableHelper = CodableHelper(),
+        successfulStatusCodeRange: Range<Int> = 200..<300,
+        interceptor: OpenAPIInterceptor = DefaultOpenAPIInterceptor(),
+    ) {
+        self.basePath = basePath
+        self.customHeaders = customHeaders
+        self.credential = credential
+        self.requestBuilderFactory = requestBuilderFactory
+        self.apiResponseQueue = apiResponseQueue
+        self.codableHelper = codableHelper
+        self.successfulStatusCodeRange = successfulStatusCodeRange
+        self.interceptor = interceptor
+    }
+
+    public static let shared = OpenAPIClientAPIConfiguration()
+}
