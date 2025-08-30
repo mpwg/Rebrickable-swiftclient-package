d56664d40c91b319f4c8a868c8a1af3726d76c5c
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2025-08-30 13:10:24 +0200

    rename (#1)
    * rename
    
    * Update README.md

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "OpenAPIClient",
+    name: "RebrickableClient",
     platforms: [
         .iOS(.v18),
         .macOS(.v15),
@@ -13,8 +13,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "OpenAPIClient",
-            targets: ["OpenAPIClient"]
+            name: "RebrickableClient",
+            targets: ["RebrickableClient"]
         )
     ],
     dependencies: [
@@ -24,9 +24,9 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "OpenAPIClient",
+            name: "RebrickableClient",
             dependencies: [],
-            path: "Sources/OpenAPIClient"
+            path: "Sources/RebrickableClient"
         )
     ],
     swiftLanguageModes: [.v6]
